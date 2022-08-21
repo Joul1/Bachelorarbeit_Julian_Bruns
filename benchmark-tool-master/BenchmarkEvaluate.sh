29567aae7ecb95022a6891ef87e9cad4dedded9d
Joul1 <74826724+Joul1@users.noreply.github.com>
2022-08-21 18:16:16 +0200

    Benchmak shell scripst

---
+++ benchmark-tool-master/BenchmarkEvaluate.sh
@@ -0,0 +1,2 @@
+./beval runscripts/runscript-1.xml > benchmark-evaluated.xml  
+./bconv "-m time:t,choices:t,conflicts:t" benchmark-evaluated.xml > results.ods
---
+++ benchmark-tool-master/BenchmarkSubmit.sh
@@ -0,0 +1,4 @@
+conda activate clingoEnv
+cd benchmark-tool-master/
+./bgen runscripts/runscript-1.xml
+./results/clingo-pbs-job/zuse/start.sh
