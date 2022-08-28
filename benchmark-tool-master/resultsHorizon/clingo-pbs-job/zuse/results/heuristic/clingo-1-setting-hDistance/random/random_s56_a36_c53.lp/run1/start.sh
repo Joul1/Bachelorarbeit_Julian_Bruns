224b75d424e2f59a907793e45eb9f79143c42a7f
jubruns <jubruns@hpc-login.hpc.cs.uni-potsdam.de>
2022-08-28 16:23:52 +0200

    Horizon Results

---
+++ benchmark-tool-master/resultsHorizon/clingo-pbs-job/zuse/results/heuristic/clingo-1-setting-hDistance/random/random_s56_a36_c53.lp/run1/start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# http://www.cril.univ-artois.fr/~roussel/runsolver/
+
+cd "$(dirname $0)"
+
+#top -n 1 -b > top.txt
+
+[[ -e .finished ]] || "../../../../../../../../../programs/runlim" \
+	--output-file=runsolver.watcher --space-limit=32000 \
+	--time-limit=21600 \
+	"../../../../../../../../../programs/clingo-1" /mnt/beegfs/home/jubruns/encoding/mifHorizon.lp --quiet=1,0 ../../../../../../../../../benchmarks/instances/random/random_s56_a36_c53.lp > runsolver.solver 
+
+touch .finished
---
+++ benchmark-tool-master/resultsHorizon/clingo-pbs-job/zuse/results/heuristic/clingo-1-setting-hDistance/room/room_s40_a20_w4.lp/run1/start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# http://www.cril.univ-artois.fr/~roussel/runsolver/
+
+cd "$(dirname $0)"
+
+#top -n 1 -b > top.txt
+
+[[ -e .finished ]] || "../../../../../../../../../programs/runlim" \
+	--output-file=runsolver.watcher --space-limit=32000 \
+	--time-limit=21600 \
+	"../../../../../../../../../programs/clingo-1" /mnt/beegfs/home/jubruns/encoding/mifHorizon.lp --quiet=1,0 ../../../../../../../../../benchmarks/instances/room/room_s40_a20_w4.lp > runsolver.solver 
+
+touch .finished
