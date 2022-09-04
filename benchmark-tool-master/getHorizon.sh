3e94ba44b3f372955171ccb53098ed101a9a8662
Joul1 <74826724+Joul1@users.noreply.github.com>
2022-09-04 15:37:56 +0200

    Instances with horizon

--- benchmark-tool-master/getHorizon.sh
+++ benchmark-tool-master/getHorizon.sh
@@ -3,7 +3,17 @@ for f in ./resultsHorizon/clingo-pbs-job/zuse/results/heuristic/clingo-1-setting
 do
 
 echo ${f}
-grep -E "query" ${f}
+#grep -E "query" ${f} | tr -s "query(" "#const="
+t=$(grep -E "query" ${f})
+
+t2=$(echo "#const horizon="${t:6})
+
+
+t3=$(echo ${t2} | tr ")" ".")
+
+echo ${t3}
+
+#const horizon=1.
 
 done
 
